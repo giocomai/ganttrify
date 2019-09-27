5d7dc8e18f82df0b1671888e9f4304c70baa3eaa
Giorgio Comai <g@giorgiocomai.eu>
2019-09-27 10:09:36 +0200

    functioning shiny

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -3,20 +3,31 @@ server <- function(input, output) {
   
   current_project_df <- shiny::eventReactive(input$project_file, {
     
-    in_file <- input$project_file
+    req(input$project_file)
     
-    if (is.null(in_file)) return(NULL)
-    
-    readr::read_csv(file = in_file$datapath)
+    readr::read_csv(file = input$project_file$datapath)
   })
   
   current_spots_df <- shiny::eventReactive(input$spot_file, {
     
-    in_file <- input$spot_file
+    req(input$spot_file)
     
-    if (is.null(in_file)) return(NULL)
-    
-    readr::read_csv(file = in_file$datapath)
+    readr::read_csv(file = input$spot_file$datapath)
+  })
+  
+  shiny::observeEvent(eventExpr = input$project_file, {
+    output$gantt <- renderPlot({
+      ganttrify::ganttrify(df = current_project_df(),
+                           start_date = input$start_date)
+    })
+  })
+  
+  shiny::observeEvent(eventExpr = input$spot_file, {
+    output$gantt <- renderPlot({
+      ganttrify::ganttrify(df = current_project_df(),
+                           spots = current_spots_df(),
+                           start_date = input$start_date)
+    })
   })
     
   
@@ -29,16 +40,13 @@ server <- function(input, output) {
   output$current_spots_table <- renderTable(
     if (is.null(current_spots_df())==FALSE) {
       current_spots_df()
-    }
+    } 
   )
 
   output$gantt <- renderPlot({
-
-    if (is.null(current_project_df())==FALSE) {
       ganttrify::ganttrify(df = current_project_df(),
-                           spots = current_spots_df(),
-                           start_date = input$start_date)
-    }
+                           start_date = input$start_date,
+                           spots = current_spots_df())
   })
   
 }
