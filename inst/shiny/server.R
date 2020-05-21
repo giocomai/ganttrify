4097207dbcd2959e407bc6631533a03f0e8f3d0a
Giorgio Comai <g@giorgiocomai.eu>
2020-05-21 14:52:38 +0200

    significantly improved functionality of sihny app

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,35 +1,64 @@
 server <- function(input, output) {
   
-  
-  current_project_df <- shiny::eventReactive(input$project_file, {
+  current_project_df <- shiny::eventReactive(input$go, {
     
-    req(input$project_file)
-    
-    readr::read_csv(file = input$project_file$datapath)
-  })
+    if (input$source_type=="demo") {
+      ganttrify::test_project
+    } else if (input$source_type=="csv") {
+      req(input$project_file)
+      readr::read_csv(file = input$project_file$datapath)
+    } else if (input$source_type=="googledrive") {
+      googlesheets4::gs4_deauth()
+      googlesheets4::read_sheet(ss = input$googledrive_link,
+                                sheet = 1,
+                                col_names = TRUE,
+                                col_types = "c")
+    }
+  }, ignoreNULL = FALSE)
   
-  current_spots_df <- shiny::eventReactive(input$spot_file, {
-    
-    req(input$spot_file)
-    
-    readr::read_csv(file = input$spot_file$datapath)
-  })
+  current_spots_df <- shiny::eventReactive(input$go, {
+    if (input$source_type=="demo") {
+      ganttrify::test_spots
+    } else if (input$source_type=="csv") {
+      req(input$spot_file)
+      readr::read_csv(file = input$spot_file$datapath)
+    } else if (input$source_type=="googledrive") {
+      googlesheets4::gs4_deauth()
+      googlesheets4::read_sheet(ss = input$googledrive_link,
+                                sheet = 2,
+                                col_names = TRUE,
+                                col_types = "c")
+    }
+  }, ignoreNULL = FALSE)
+
+  output$current_project_table <- renderTable(
+    if (input$go==0) {
+      ganttrify::test_project
+    } else if (is.null(current_project_df())==FALSE) {
+      current_project_df()
+    },
+    striped = TRUE,
+    hover = TRUE, 
+    digits = 0,
+    align = "c",
+    width = "100%")
   
-  shiny::observeEvent(eventExpr = input$project_file, {
-    output$gantt <- renderPlot({
-      ganttrify::ganttrify(df = current_project_df(),
-                           start_date = input$start_date,
-                           mark_quarters = input$mark_quarters,
-                           month_number = input$month_number,
-                           size_wp = input$size_wp,
-                           size_activity = input$size_activity,
-                           size_text_relative = input$size_text_relative/100
-      )
-    })
-  })
   
-  shiny::observeEvent(eventExpr = input$spot_file, {
-    output$gantt <- renderPlot({
+  
+  output$current_spots_table <- renderTable(
+    if (input$go==0) {
+      ganttrify::test_spots
+    } else if (is.null(current_spots_df())==FALSE) {
+      current_spots_df()
+    },
+    striped = TRUE,
+    hover = TRUE,
+    digits = 0,
+    align = "c",
+    width = "100%"
+  )
+
+  gantt_chart <- shiny::reactive({
       ganttrify::ganttrify(df = current_project_df(),
                            start_date = input$start_date,
                            spots = current_spots_df(),
@@ -37,37 +66,12 @@ server <- function(input, output) {
                            month_number = input$month_number,
                            size_wp = input$size_wp,
                            size_activity = input$size_activity,
-                           size_text_relative = input$size_text_relative/100
-      )
-    })
-  })
-    
-  
-  output$current_project_table <- renderTable(
-    if (is.null(current_project_df())==FALSE) {
-      current_project_df()
-      }
-  )
-  
-  output$current_spots_table <- renderTable(
-    if (is.null(current_spots_df())==FALSE) {
-      current_spots_df()
-    } 
-  )
-
-  gantt_chart <- shiny::reactive({
-    ganttrify::ganttrify(df = current_project_df(),
-                         start_date = input$start_date,
-                         spots = current_spots_df(),
-                         mark_quarters = input$mark_quarters,
-                         month_number = input$month_number,
-                         size_wp = input$size_wp,
-                         size_activity = input$size_activity,
-                         size_text_relative = input$size_text_relative/100
-    )
+                           size_text_relative = input$size_text_relative/100,
+                           colour_palette = unlist(ifelse(test = input$custom_palette_check, strsplit(input$custom_palette_text, split = ","), list(as.character(wesanderson::wes_palette(input$wes_palette))))))
   })
   
   output$gantt <- renderPlot({
+    
     gantt_chart()
   })
   
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,31 +1,100 @@
 ui <- fluidPage(
+  tags$link(rel = "stylesheet", type = "text/css", href = "css/roboto.css"),
+  tags$link(rel = "stylesheet", type = "text/css", href = "css/satisfy.css"),
+  tags$style("body {font-family: 'Roboto', sans-serif;}"),
+  tags$style("h2 {font-family: 'Satisfy', sans-serif;
+  font-size: 50px;
+  text-align: center;
+  color: #6f2c91;
+  text-shadow: 3px 3px 3px #grey; 
+  text-decoration: none;}"),
+  tags$style("h2 a {text-align:center;
+  font-size: 50px;
+  text-align: center;
+  color: #6f2c91;
+  text-shadow: 3px 3px 3px #grey;}"),
+  tags$style("h2 a:hover {text-align:center;
+  font-size: 50px;
+  text-align: center;
+  color: #a6ce39;
+  text-shadow: 3px 3px 3px #grey;}"),
+  tags$style("h3 a {font-family: 'Satisfy', sans-serif; fonts-size: 30px; text-align: center; color: #6f2c91; text-shadow: 3px 3px 3px #grey;"),
+  tags$style("h3 a:hover {font-family: 'Satisfy', sans-serif; fonts-size: 30px; text-align: center; color: #a6ce39; text-shadow: 3px 3px 3px #grey;"),
+  tags$style("h3 {font-family: 'Satisfy', sans-serif; fonts-size: 30px; text-align: center; color: #a6ce39; text-shadow: 3px 3px 3px #grey;"),
+  tags$style("h4 {font-family: 'Roboto', sans-serif; font-weight: bold; text-shadow: 3px 3px 3px #grey;text-decoration: underline;"),
+  tags$style(".col-sm-8 {text-align: center;}"),
+  tags$style(".col-sm-8 table {text-align: center;}"),
+  tags$style("#current_project_table {width:95%;text-align: center;}"),
+  tags$style("#current_spots_table {width:95%;text-align: center;}"),
   
-  titlePanel("Ganttrify!"),
   
+  shiny::titlePanel(title = tags$a("Ganttrify!", href='https://github.com/giocomai/ganttrify/'),
+                    windowTitle = "Ganttrify - A tool by EDJNet"),
+
   sidebarLayout(
     sidebarPanel(
-      shiny::fileInput(inputId = "project_file", label = "Upload csv of project"),
-      shiny::fileInput(inputId = "spot_file", label = "Upload csv of spots"),
-      shiny::dateInput(inputId = "start_date", label = "Starting date of the project"),
+      shiny::radioButtons(inputId = "source_type",
+                          label = "Source for project information",
+                          choices = c(demo = "demo",
+                                      `CSV files` = "csv",
+                                      `Google Drive` = "googledrive")),
+      
+      conditionalPanel(
+        condition = "input.source_type == 'csv'",
+        shiny::fileInput(inputId = "project_file", label = "Upload csv of project"),
+        shiny::fileInput(inputId = "spot_file", label = "Upload csv of spots")),
+      conditionalPanel(
+        condition = "input.source_type == 'googledrive'",
+        shiny::textInput(inputId = "googledrive_link",
+                         label = "Link to Google Drive spreadsheet",
+                         value = "https://docs.google.com/spreadsheets/d/1fgrsCqtGaV5sd_fD48VwBLWCANEvyV-Di7RTKC6Yo7o/"),
+        shiny::helpText("Note: the Google Drive spreadsheet must be visibile by anyone with the link; to have a properly structured spreadsheet, the easiest way is to copy the example document: 'File' -> 'Create a copy'")
+        ),
+   
+      
+      shiny::textInput(inputId = "start_date", label = "Starting date of the project", value = substring(text = as.character(Sys.Date()), first = 1, last = 7)),
       shiny::checkboxInput(inputId = "month_number", label = "Include month numbers on top", value = TRUE),
-      shiny::checkboxInput(inputId = "mark_quarters", label = "Add vertical lines to mark quarters", value = FALSE),
-      shiny::numericInput(inputId = "size_wp", label = "Thickness of the line for working packages", value = 6),
-      shiny::numericInput(inputId = "size_activity", label = "Thickness of the line for activities", value = 4),
-      shiny::sliderInput(inputId = "size_text_relative", label = "Relative size of all text", value = 100, min = 1, max = 500, round = TRUE, post = "%"),
+      shiny::checkboxInput(inputId = "mark_quarters", label = "Add vertical lines to mark quarters", value = TRUE),
+      shiny::sliderInput(inputId = "size_wp", label = "Thickness of the line for working packages", min = 1, max = 10, value = 6, step = 1, round = TRUE),
+      shiny::sliderInput(inputId = "size_activity", label = "Thickness of the line for activities", min = 1, max = 10, value = 4, step = 1, round = TRUE),
+      shiny::sliderInput(inputId = "size_text_relative", label = "Relative size of all text", value = 100, min = 20, max = 200, round = TRUE, post = "%"),
+      shiny::selectInput(inputId = "wes_palette", label = "Pick colour palette", choices = names(wesanderson::wes_palettes), selected = "Darjeeling1", multiple = FALSE),
+      shiny::checkboxInput(inputId = "custom_palette_check", label = "Custom colour palette?", value = FALSE),
+      conditionalPanel(
+        condition = "input.custom_palette_check == true",
+        shiny::textInput(inputId = "custom_palette_text", label = "Custom hex values", value = "#a6ce39,#6f2c91,#a6ce39"),
+        shiny::helpText("Insert comma-separated hex values, with no spaces.")),
       shiny::HTML("<hr />"),
-      shiny::numericInput(inputId = "download_width",
-                          label = "Download width (in cm)",
-                          value = 18),
-      shiny::numericInput(inputId = "download_height",
-                          label = "Download height (in cm)",
-                          value = 13.5),
-      shiny::downloadButton(outputId = "download_gantt",
-                     label =  "Download Gantt chart")
+      shiny::sliderInput(inputId = "download_width",
+                         label = "Download width (in cm)",
+                         value = 18,
+                         min = 5,
+                         max = 50,
+                         step = 1,
+                         round = TRUE),
+      shiny::sliderInput(inputId = "download_height",
+                         label = "Download height (in cm)",
+                         value = 9,
+                         min = 5,
+                         max = 50,
+                         step = 1,
+                         round = TRUE),
+      shiny::h3(tags$a("A tool by EDJNet", href='https://www.europeandatajournalism.eu/')),
+      shiny::h3(tags$a(tags$img(src = "img/edjnet_logo_full.svg"), href='https://www.europeandatajournalism.eu/'))
+      
     ),
     mainPanel(
+      shiny::actionButton("go", "Update chart"),
       shiny::plotOutput(outputId = "gantt"),
+      shiny::downloadButton(outputId = "download_gantt",
+                            label =  "Download chart"),
+      shiny::hr(),
+      shiny::h4("Project data"), 
       shiny::tableOutput(outputId = "current_project_table"),
-      shiny::tableOutput(outputId = "current_spots_table")
+      shiny::h4("Spot occurrences"), 
+      shiny::tableOutput(outputId = "current_spots_table"),
+
     )
   )
+  
 )
