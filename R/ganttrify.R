f8816cf4d4f52d456ada31d579ad185aa43b8ddc
Giorgio Comai <g@giorgiocomai.eu>
2024-01-22 20:19:28 +0100

    fix when label_wrap enabled, and spots included, either as spots on wp or activity

--- R/ganttrify.R
+++ R/ganttrify.R
@@ -146,17 +146,6 @@ ganttrify <- function(project,
     line_end_activity <- line_end
   }
 
-  if (label_wrap != FALSE) {
-    if (isTRUE(label_wrap)) {
-      label_wrap <- 32
-    }
-    project$wp <- stringr::str_wrap(string = project$wp, width = label_wrap)
-    project$activity <- stringr::str_wrap(string = project$activity, width = label_wrap)
-    if (is.null(spots) == FALSE) {
-      spots$activity <- stringr::str_wrap(string = spots$activity, width = label_wrap)
-    }
-  }
-
   if (by_date == FALSE) {
     df <- project %>%
       dplyr::mutate(
@@ -279,13 +268,34 @@ ganttrify <- function(project,
   if (wp_label_bold) {
     distinct_yearmon_labels_df <- distinct_yearmon_labels_df %>%
       dplyr::mutate(wp = stringr::str_c("<b>", wp, "</b>"))
-  }
 
+    if (is.null(spots) == FALSE) {
+      wp_v <- project %>%
+        dplyr::distinct(wp) %>%
+        dplyr::pull(wp)
+
+      spots[["activity"]][spots[["activity"]] %in% wp_v] <- stringr::str_c("<b>", spots[["activity"]][spots[["activity"]] %in% wp_v], "</b>")
+    }
+  }
 
   level_labels_df <- tibble::tibble(
     levels = rev(unlist(t(matrix(c(distinct_yearmon_levels_df$wp, distinct_yearmon_levels_df$wp_activity), ncol = 2)))),
     labels = rev(unlist(t(matrix(c(distinct_yearmon_labels_df$wp, distinct_yearmon_labels_df$activity), ncol = 2))))
   )
+
+  if (label_wrap != FALSE) {
+    if (isTRUE(label_wrap)) {
+      label_wrap <- 32
+    }
+
+    level_labels_df$labels <- stringr::str_wrap(string = level_labels_df$labels, width = label_wrap)
+    level_labels_df$labels <- stringr::str_replace_all(string = level_labels_df$labels, pattern = "\n", replacement = "<br />")
+
+    if (is.null(spots) == FALSE) {
+      spots$activity <- stringr::str_wrap(string = spots$activity, width = label_wrap)
+      spots$activity <- stringr::str_replace_all(string = spots$activity, pattern = "\n", replacement = "<br />")
+    }
+  }
 
   if (exact_date == TRUE) {
     df_yearmon_fct <-
@@ -425,6 +435,8 @@ ganttrify <- function(project,
   } else {
     axis_text_align_n <- 1
   }
+
+
 
   gg_gantt <- gg_gantt +
     ggplot2::scale_y_discrete(
